a62a7b5b993782784397fa2f86eef3a99a480688
Carlos <carlosahs123@gmail.com>
2025-05-20 22:44:18 -0600

    Diseno en boton PDF y Menu

--- INVENTARIOAPP/core/settings.py
+++ INVENTARIOAPP/core/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
